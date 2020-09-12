06ed7e4e0fd622731e74f156999237258ae69bb1
basdxz <tudurap.com@gmail.com>
2020-09-12 19:46:43 +0100

    Lots of stuff, read more below :p
    -Added build.properties
    -Added git commit version appendage to the jar
    -Worked towards unifying build settings into one file
    -As a feature, build.properties now defines mod id, mod name and mod version everywhere

--- src/main/java/com/github/basdxz/examplemod/ExampleMod.java
+++ src/main/java/com/github/basdxz/examplemod/ExampleMod.java
@@ -5,16 +5,13 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
+import static com.github.basdxz.examplemod.Reference.*;
+
+@Mod(modid = MODID, name = Reference.NAME, version = VERSION)
+public class ExampleMod {
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
     }
 }
---
+++ src/main/java/com/github/basdxz/examplemod/Reference.java
@@ -0,0 +1,9 @@
+package com.github.basdxz.examplemod;
+
+public final class Reference {
+    public static final String MODID = "@MODID@";
+    public static final String NAME = "@NAME@";
+    public static final String VERSION = "@VERSION@";
+
+    private Reference() {}
+}
