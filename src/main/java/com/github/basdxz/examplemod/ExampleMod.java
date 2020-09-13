e42935f34d3841b997d9ccbf167759edb451ad75
basdxz <tudurap.com@gmail.com>
2020-09-13 02:14:18 +0100

    Added example to Shadow jars

--- src/main/java/com/github/basdxz/examplemod/ExampleMod.java
+++ src/main/java/com/github/basdxz/examplemod/ExampleMod.java
@@ -5,6 +5,8 @@ import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
+import reactor.core.publisher.Flux;
+
 import static com.github.basdxz.examplemod.Reference.*;
 
 @Mod(modid = MODID, name = Reference.NAME, version = VERSION)
@@ -12,6 +14,7 @@ public class ExampleMod {
     @EventHandler
     public void init(FMLInitializationEvent event) {
         // some example code
-        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+        String e = Flux.class.getSimpleName();
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName() + " WOAG " + e);
     }
 }
